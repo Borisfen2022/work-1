249c83096c820c6cc1dfb146b1bdc099fe4aa916
Olha_koshelnikova <olha.koshelnikova@gmail.com>
2022-04-14 18:43:42 +0300

    OL-6  last renew

--- js/index.js
+++ js/index.js
@@ -1,29 +1,31 @@
 const handleSwitchClick = () => {
   const round = document.querySelector(".round");
+  const annually = document.querySelector(".annually");
+  const monthly = document.querySelector(".monthly");
 
   if (round.classList.contains("round-off")) {
     round.classList.remove("round-off");
     round.classList.add("round-on");
+    annually.classList.add("selected");
+    monthly.classList.remove("selected");
   } else {
     round.classList.remove("round-on");
     round.classList.add("round-off");
+    annually.classList.remove("selected");
+    monthly.classList.add("selected");
   }
 };
 
-const handleMonthlyClick = (num) => {
-  alert(num);
-  const monthlyLabel = document.querySelector(".monthly");
-
-  if (monthlyLabel.classList.contains("not-selected")) {
-    monthlyLabel.classList.remove("not-selected");
-  }
-
-  let temp = 19;
-  console.log(temp);
-
-  getTemp(temp);
+const handleMonthlyClick = () => {
+  const monthly = document.querySelector(".monthly");
+  const annually = document.querySelector(".annually");
+  monthly.classList.add("selected");
+  annually.classList.remove("selected");
 };
 
-function getTemp {
-  console.log(arg);
-}
+const handleAnnualyClick = () => {
+  const annually = document.querySelector(".annually");
+  const monthly = document.querySelector(".monthly");
+  annually.classList.add("selected");
+  monthly.classList.remove("selected");
+};
